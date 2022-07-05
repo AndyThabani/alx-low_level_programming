7f6df521249e9b648551d1089787e27d043cf06b
AndrewThabani <andreufish@gmail.com>
2022-07-05 05:16:26 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints putchar
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "main";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
